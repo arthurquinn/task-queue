5ef94589fed98c93d859d00e0ebf109ab6f91776
ArthurQ <artquint@rutgers.edu>
2017-09-19 09:40:02 -0400

    remove launch process in new thread code from udp branch

--- include/dispatcher.h
+++ include/dispatcher.h
@@ -5,26 +5,17 @@
 
 #include "queue_item.h"
 
-#include <pthread.h>
 #include <vector>
 #include <iostream>
-
-void* thread_run(void* args);
 
 class Dispatcher {
 private:
   std::vector<QueueItem*> stage;
-  void start_in_thread(QueueItem* queue_item);
 public:
   Dispatcher();
   const int count() const;
   void dispatch(QueueItem* queue_item);
   ~Dispatcher();
 };
-
-typedef struct thread_args {
-  Dispatcher* instance;
-  QueueItem* queue_item;
-} thread_args_t;
 
 #endif
--- src/dispatcher.cpp
+++ src/dispatcher.cpp
@@ -1,34 +1,5 @@
 #include "dispatcher.h"
 
-void* thread_run(void* args) {
-  thread_args_t* thread_args = static_cast<thread_args_t*>(args);
-  QueueItem* qi = thread_args->queue_item;
-  std::cout << static_cast<int>(qi->priority()) << std::endl;
-
-  const char* cmd = static_cast<const char*>(qi->data());
-  std::cout << "about to execute cmd: " << cmd << std::endl;
-  system(cmd);
-
-  delete thread_args;
-  pthread_exit(NULL);
-}
-
-// PRIVATE MEMBERS
-
-void Dispatcher::start_in_thread(QueueItem* queue_item) {
-  thread_args_t* args = new thread_args_t();
-  args->instance = this;
-  args->queue_item = queue_item;
-
-  pthread_t thread;
-  pthread_attr_t thread_attr;
-  pthread_attr_init(&thread_attr);
-  pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
-  pthread_attr_setscope(&thread_attr, PTHREAD_SCOPE_SYSTEM);
-  pthread_create(&thread, &thread_attr, &thread_run, static_cast<void*>(args));
-  pthread_attr_destroy(&thread_attr);
-  std::cout << "dispatched!!" << std::endl;
-}
 
 // PUBLIC MEMBERS
 
@@ -45,7 +16,7 @@ void Dispatcher::dispatch(QueueItem* item) {
     throw std::length_error("dispatcher: maximum number of tasks dispatched reached");
   }
   stage.push_back(item);
-  start_in_thread(item);
+
 }
 
 Dispatcher::~Dispatcher() {
