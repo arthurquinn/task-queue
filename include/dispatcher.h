cb80400ad524bb6cb1164f845fcac485c3f4ad8d
ArthurQ <artquint@rutgers.edu>
2017-09-19 09:49:54 -0400

    moved udp listen log to dispatcher

--- include/dispatcher.h
+++ include/dispatcher.h
@@ -2,19 +2,45 @@
 #define DISPATCHER_H
 
 #define STAGE_SIZE 4
-
-#include "queue_item.h"
+#define UDP_PORT_NO 12950
+#define UDP_BUFFER_SIZE 2048
 
 #include <vector>
 #include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#include "queue_item.h"
+
+typedef int socket_t;
+
+// Listen in a loop for messages received on the udp socket
+// This method listens in a thread and closes out processes that have written back their pids
+void* listen_udp_socket(void*);
 
 class Dispatcher {
 private:
   std::vector<QueueItem*> stage;
+  socket_t udp_socket;
+
+  // Friend function that will run in a separate thread and listen on a udp socket for processes writing back their
+  // process id's after they have finished execution
+  friend void* listen_udp_socket(void*);
 public:
+
+  // Constructs a dispatcher that will handle process starting and ending 
   Dispatcher();
+
+  // Returns the number of currently staged queue items
+  // Queue items on the stage are items that are either starting to run, running, or have finished but have not yet been
+  // unstages by the dispatcher
   const int count() const;
+
+  // Dispatch a queue item to begin running its target process
+  // This will call the command specified by the queue item
   void dispatch(QueueItem* queue_item);
+
   ~Dispatcher();
 };
 
--- src/task_manager.cpp
+++ src/task_manager.cpp
@@ -1,49 +1,7 @@
 #include "task_manager.h"
 
-void* listen_udp_socket(void* args) {
-  TaskManager* instance = static_cast<TaskManager*>(args);
-  socket_t udp_socket = instance->udp_socket;
-
-  struct sockaddr_in remote_addr;
-  socklen_t remote_len = sizeof(struct sockaddr_in);
-  unsigned char buffer[UDP_BUFFER_SIZE];
-  int recvlen;
-
-  for (;;) {
-    std::cout << "listening for messages on port " << UDP_PORT_NO << std::endl;
-    recvlen = recvfrom(udp_socket, buffer, UDP_BUFFER_SIZE, 0, reinterpret_cast<struct sockaddr*>(&remote_addr), &remote_len);
-    std::cout << "received " << recvlen << " bytes" << std::endl;
-    if (recvlen > 0) {
-      buffer[recvlen] = 0;
-      std::cout << "received message: " << buffer << std::endl;
-    }
-  }
-
-  std::cout << "leaving listen_udp_socket" << std::endl;
-}
-
 TaskManager::TaskManager(const unsigned int max_tasks, const std::string& recover_dir) : queue(max_tasks, recover_dir) {
-  if ((udp_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    throw std::runtime_error("task manager: could not initiate udp socket");
-  }
 
-  struct sockaddr_in sock_addr;
-  memset(reinterpret_cast<char*>(&sock_addr), 0, sizeof(struct sockaddr_in));
-  sock_addr.sin_family = AF_INET;
-  sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  sock_addr.sin_port = htons(UDP_PORT_NO);
-
-  if (bind(udp_socket, reinterpret_cast<struct sockaddr*>(&sock_addr), sizeof(struct sockaddr_in)) < 0) {
-    throw std::runtime_error("task manager: could not bind udp_socket");
-  }
-
-  pthread_t thread;
-  pthread_attr_t thread_attr;
-  pthread_attr_init(&thread_attr);
-  pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
-  pthread_attr_setscope(&thread_attr, PTHREAD_SCOPE_SYSTEM);
-  pthread_create(&thread, &thread_attr, &listen_udp_socket, static_cast<void*>(this));
-  pthread_attr_destroy(&thread_attr);
 }
 
 const int TaskManager::stage_task() {
